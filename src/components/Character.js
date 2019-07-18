933fc700054c59bfc0102fd2357c0ba8dd1d4568
Heather Zurek <heather.zurek@gmail.com>
2019-07-18 16:33:39 -0700

    updated character.js file

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,101 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
+import { useHttp } from '../hooks/http';
 import Summary from './Summary';
 
-
 const Character = props => {
-  const [loadedCharacter, setLoadedCharacter] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-  
-  console.log('is rendering...');
 
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   console.log('shouldComponentUpdate');
-  //   return (
-  //     nextProps.selectedChar !== props.selectedChar ||
-  //     nextState.loadedCharacter.id !== loadedCharacter.id ||
-  //     nextState.isLoading !== isLoading
-  //   );
-  // }
-  
-  const fetchData = () => {
-    console.log(
-      'Sending Http request for new character with id ' +
-        props.selectedChar
-    );
-    setIsLoading(true);
-    fetch('https://swapi.co/api/people/' + props.selectedChar)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Could not fetch person!');
-        }
-        return response.json();
-      })
-      .then(charData => {
-        const loadedCharacter = {
-          id: props.selectedChar,
-          name: charData.name,
-          height: charData.height,
-          colors: {
-            hair: charData.hair_color,
-            skin: charData.skin_color
-          },
-          gender: charData.gender,
-          movieCount: charData.films.length
-        };
-        setIsLoading(false);
-        setLoadedCharacter(loadedCharacter);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+  const [isLoading, fetchedData] = useHttp(
+    'https://swapi.co/api/people/' + props.selectedChar,
+    [props.selectedChar]
+  );
 
-  // componentDidUpdate(prevProps) {
-  //   console.log('Component did update');
-  //   if (prevProps.selectedChar !== props.selectedChar) {
-  //     this.fetchData();
-  //   }
-  // }
+  let loadedCharacter = null;
 
-  // useEffect(() => {
-  //   fetchData()
-  // }, []);
-
-  useEffect(() => {
-    fetchData();
-    return () => {
-      console.log('Cleaning up...');
+  if (fetchedData) {
+    loadedCharacter = {
+      id: props.selectedChar,
+      name: fetchedData.name,
+      height: fetchedData.height,
+      colors: {
+        hair: fetchedData.hair_color,
+        skin: fetchedData.skin_color
+      },
+      gender: fetchedData.gender,
+      movieCount: fetchedData.films.length
     };
-  }, [props.selectedChar]);
+  }
 
   useEffect(() => {
     return () => {
-      console.log('Component did unmount');
-    }
+      console.log('component did unmount');
+    };
   }, []);
 
-  // componentWillUnmount() {
-  //   console.log('Too soon...');
-  // }
+  let content = <p>Loading Character...</p>;
 
-    let content = <p>Loading Character...</p>;
-
-    if (!isLoading && loadedCharacter.id) {
-      content = (
-        <Summary
-          name={loadedCharacter.name}
-          gender={loadedCharacter.gender}
-          height={loadedCharacter.height}
-          hairColor={loadedCharacter.colors.hair}
-          skinColor={loadedCharacter.colors.skin}
-          movieCount={loadedCharacter.movieCount}
-        />
-      );
-    } else if (!isLoading && !loadedCharacter.id) {
-      content = <p>Failed to fetch character.</p>;
-    }
-    return content;
+  if (!isLoading && loadedCharacter) {
+    content = (
+      <Summary
+        name={loadedCharacter.name}
+        gender={loadedCharacter.gender}
+        height={loadedCharacter.height}
+        hairColor={loadedCharacter.colors.hair}
+        skinColor={loadedCharacter.colors.skin}
+        movieCount={loadedCharacter.movieCount}
+      />
+    );
+  } else if (!isLoading && !loadedCharacter) {
+    content = <p>Failed to fetch character.</p>;
   }
+  return content;
+};
 
 export default React.memo(Character);
